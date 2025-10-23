1ad5e6680e1f57cb00a0dd0d4901050088d99803
Mira <mira.bunciuc@gmail.com>
2025-10-23 12:17:24 +0100

    small changes

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,22 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
       </div>
       <h1>SecureShare</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
     </>
   )
 }
